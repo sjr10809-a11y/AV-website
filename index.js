a997e95934102cae64a7e338ea3d4506a795edd2
sjr10809-a11y <sjr10809@gmail.com>
2026-05-04 08:35:47 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+const playBothBtn = document.querySelector('#play-both-button'); 
+//we use const instead of let simply because the button wont change, it will always be this button, however, let will work the same way 
+let videoOne = document.querySelector("#video-one");
+let audioOne = document.querySelector("#audio-one");
+//we also save the video and audio into variables here so that we can affect them later
+
+playBothBtn.addEventListener("click",function(){
+  console.log("both button has been clicked ( ^ _ ^ )");
+});
